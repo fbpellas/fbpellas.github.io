486a3fca06c1b427322f9e33cb292dbd3dff93e5
sebranly <sebranly.pinf@gmail.com>
2020-10-31 01:01:59 +0100

    feat: add initial commit from sebranly.github.io

---
+++ src/search.ts
@@ -0,0 +1,85 @@
+import {
+  diphthongs,
+  consonants,
+  longVowels,
+  shortVowels,
+  rColoredVowels,
+} from "./phonemes";
+import flatten from "lodash/flatten";
+
+const generateKeys = (data: any) => {
+  return flatten(
+    data.map((el: any) => {
+      const examples = el[2];
+      const sanitizedExamples = examples
+        .replaceAll("<u>", "")
+        .replaceAll("</u>", "")
+        .split(", ");
+
+      const filteredExamples = sanitizedExamples.filter(
+        (example: any) => example.length > 2
+      );
+
+      return filteredExamples;
+    })
+  );
+};
+
+const consonantsKeys = generateKeys(consonants);
+const diphthongsKeys = generateKeys(diphthongs);
+const longVowelsKeys = generateKeys(longVowels);
+const rColoredVowelsKeys = generateKeys(rColoredVowels);
+const shortVowelsKeys = generateKeys(shortVowels);
+
+export const mapping = [
+  [
+    ["about", "author", "about the author", "faith", "pellas", "faith pellas"],
+    "about-author",
+    "About the Author",
+  ],
+  [
+    ["letter", "teachers", "how to", "letter to teachers", "letter teachers"],
+    "letter-teachers",
+    "Letter to Teachers",
+  ],
+  [
+    [...consonantsKeys, "consonant", "consonants", "letter", "letters"],
+    "consonants",
+    "Consonants",
+  ],
+  [["phoneme", "phonemes"], "phonemes", "Phonemes"],
+  [["letter", "letter", "vowel", "vowels"], "vowels", "Vowels"],
+  [
+    [...longVowelsKeys, "vowel", "vowels", "long vowel", "long vowels"],
+    "long-vowels",
+    "Long vowels",
+  ],
+  [
+    [...shortVowelsKeys, "vowel", "vowels", "short vowel", "short vowels"],
+    "short-vowels",
+    "Short vowels",
+  ],
+  [
+    [
+      ...rColoredVowelsKeys,
+      "vowel",
+      "vowels",
+      "r-colored vowel",
+      "r-colored vowels",
+      "r colored vowel",
+      "r colored vowels",
+    ],
+    "r-colored-vowels",
+    "R-colored vowels",
+  ],
+  [[...diphthongsKeys, "diphthong", "diphthongs"], "diphthongs", "Diphthongs"],
+  [["stress"], "stress", "Stress"],
+  [["intonation"], "intonation", "Intonation"],
+  [["resource", "resources"], "resources", "Resources"],
+  [["quiz", "quizzes"], "quiz", "Quiz"],
+  [
+    ["english", "pronunciation", "english pronunciation", "main"],
+    "main",
+    "Main Page",
+  ],
+];
