bc93c7245dd2a52d0b415d33e041ecdee728f67f
sebranly <sebranly.pinf@gmail.com>
2020-11-01 23:05:49 +0100

    Updates

---
+++ precache-manifest.ae8f2dd5345c257e5f649df3de1f9379.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edbd13c61128295b274b793bd0b7e833",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf1ea1eddcac2d69200f",
+    "url": "/static/css/2.5d455929.chunk.css"
+  },
+  {
+    "revision": "f1f1d9c601423a207189",
+    "url": "/static/css/main.83bca3f0.chunk.css"
+  },
+  {
+    "revision": "bf1ea1eddcac2d69200f",
+    "url": "/static/js/2.8c4f7e32.chunk.js"
+  },
+  {
+    "revision": "3a9d301c967b4d14437611c8ed5f2839",
+    "url": "/static/js/2.8c4f7e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1f1d9c601423a207189",
+    "url": "/static/js/main.3452e110.chunk.js"
+  },
+  {
+    "revision": "a8d5059f6465c4421c6c",
+    "url": "/static/js/runtime-main.15d4b7ca.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a6ac6cd20a2b889c9b1e89dcf9e7f436.js"
+  "/precache-manifest.ae8f2dd5345c257e5f649df3de1f9379.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
