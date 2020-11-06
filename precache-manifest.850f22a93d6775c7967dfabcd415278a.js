50d0a5d487e7abb96832ea91f6283dd51aa237e7
sebranly <sebranly.pinf@gmail.com>
2020-11-06 17:45:47 +0100

    Updates

---
+++ precache-manifest.850f22a93d6775c7967dfabcd415278a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e0cbbd0f7c9e2c37c74429dd9b25586",
+    "url": "/index.html"
+  },
+  {
+    "revision": "358aa5bcac0bb0ea1a06",
+    "url": "/static/css/2.5d455929.chunk.css"
+  },
+  {
+    "revision": "87584310479594cb149d",
+    "url": "/static/css/main.dc65f0f0.chunk.css"
+  },
+  {
+    "revision": "358aa5bcac0bb0ea1a06",
+    "url": "/static/js/2.0889c177.chunk.js"
+  },
+  {
+    "revision": "3a9d301c967b4d14437611c8ed5f2839",
+    "url": "/static/js/2.0889c177.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87584310479594cb149d",
+    "url": "/static/js/main.dbee901f.chunk.js"
+  },
+  {
+    "revision": "a8d5059f6465c4421c6c",
+    "url": "/static/js/runtime-main.15d4b7ca.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5eb48a2fcb5973a64a732d6a91c4463d.js"
+  "/precache-manifest.850f22a93d6775c7967dfabcd415278a.js"
 );
 
 self.addEventListener('message', (event) => {
