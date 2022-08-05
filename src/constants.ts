d52c28d2d696656a28b2a3494d78d0cf12c07010
Sebastien Branly <sebranly.pinf@gmail.com>
2022-08-05 10:53:50 +0200

    ✨ 2022 Lift (#4)

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const BASE_PATH = `https://raw.githubusercontent.com/fbpellas/fbpellas.github.io/main/public/`;
+export const BASE_PATH_IMG = `${BASE_PATH}img/`;
+export const BASE_PATH_SOUNDS = `${BASE_PATH}sounds/`;
+export const EMAIL = "fbpellas@usfca.edu";
