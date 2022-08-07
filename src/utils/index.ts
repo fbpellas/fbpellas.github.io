4a51b385ed601f4c4fd42550bf0324f9d0a2baa3
sebranly <sebranly.pinf@gmail.com>
2022-08-07 20:00:52 +0200

    refactor: simplify

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -18,74 +18,54 @@ const titleize = (word: string) => {
  * @name generateBreadcrumbs
  * @description Based on the current page, get the path to this page
  */
-const generateBreadcrumbs = (page: string, indexCarousel: QuizIndex) => {
-  const links: Breadcrumb[] = [{ anchor: 'home', title: 'Home' }];
+const generateBreadcrumbs = (page: string, indexCarousel: QuizIndex): Breadcrumb[] => {
+  const { GuessThePattern, OddPhonemeOut, PhoneticSpelling, SameWordsDifferentStress, ShoppingForAPresent, WhereIsTheStress } = QuizIndex;
+  const title = page === 'non-final' ? 'Non-Final' : titleize(page);
+
+  const home: Breadcrumb = { anchor: 'home', title: 'Home' };
+  const phonemes: Breadcrumb = { anchor: 'phonemes', title: 'Phonemes' }
+  const intonation: Breadcrumb = { anchor: 'intonation', title: 'Intonation' }
+  const stress: Breadcrumb = { anchor: 'stress', title: 'Stress' }
+  const currentPage: Breadcrumb = { anchor: page, title };
 
   switch (page) {
     case 'about-author':
-      links.push({ anchor: page, title: 'About the Author' });
-      break;
+      return [home, { anchor: page, title: 'About the Author' }];
 
     case 'mission':
-      links.push({ anchor: page, title: 'Mission' });
-      break;
-
-    case 'phonemes':
-      links.push({ anchor: page, title: 'Phonemes' });
-      break;
-
-    case 'vowels':
-      links.push({ anchor: 'phonemes', title: 'Phonemes' });
-      links.push({ anchor: page, title: 'Vowels' });
-      break;
-
-    case 'diphthongs':
-      links.push({ anchor: 'phonemes', title: 'Phonemes' });
-      links.push({ anchor: page, title: 'Diphthongs' });
-      break;
-
-    case 'consonants':
-      links.push({ anchor: 'phonemes', title: 'Phonemes' });
-      links.push({ anchor: page, title: 'Consonants' });
-      break;
-
-    case 'stress':
-      links.push({ anchor: page, title: 'Stress' });
-      break;
+      return [home, { anchor: page, title: 'Mission' }];
 
     case 'intonation':
-      links.push({ anchor: page, title: 'Intonation' });
-      break;
+    case 'phonemes':
+    case 'stress':
+      return [home, currentPage];
+
+    case 'consonants':
+    case 'diphthongs':
+    case 'vowels':
+      return [home, phonemes, currentPage]
 
     case 'falling':
-      links.push({ anchor: 'intonation', title: 'Intonation' });
-      links.push({ anchor: page, title: 'Falling' });
-      break;
-
-    case 'rising':
-      links.push({ anchor: 'intonation', title: 'Intonation' });
-      links.push({ anchor: page, title: 'Rising' });
-      break;
-
     case 'non-final':
-      links.push({ anchor: 'intonation', title: 'Intonation' });
-      links.push({ anchor: page, title: 'Non-Final' });
-      break;
+    case 'rising':
+      return [home, intonation, currentPage];
 
     case 'quiz':
-      if ([QuizIndex.SameWordsDifferentStress, QuizIndex.WhereIsTheStress].includes(indexCarousel)) {
-        links.push({ anchor: 'stress', title: 'Stress' });
-      } else if ([QuizIndex.GuessThePattern, QuizIndex.ShoppingForAPresent].includes(indexCarousel)) {
-        links.push({ anchor: 'intonation', title: 'Intonation' });
-      } else if ([QuizIndex.OddPhonemeOut, QuizIndex.PhoneticSpelling].includes(indexCarousel)) {
-        links.push({ anchor: 'phonemes', title: 'Phonemes' });
+      if ([SameWordsDifferentStress, WhereIsTheStress].includes(indexCarousel)) {
+        return [home, stress, currentPage]
       }
 
-      links.push({ anchor: page, title: 'Quiz' });
-      break;
-  }
+      if ([GuessThePattern, ShoppingForAPresent].includes(indexCarousel)) {
+        return [home, intonation, currentPage]
+      }
 
-  return links;
+      if ([OddPhonemeOut, PhoneticSpelling].includes(indexCarousel)) {
+        return [home, phonemes, currentPage]
+      }
+
+    default:
+      return [home];
+  }
 }
 
 export { generateBreadcrumbs, titleize };
