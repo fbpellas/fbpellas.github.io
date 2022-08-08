9ccaac5a2911062a9e0e43a4abea41635e98b15d
sebranly <sebranly.pinf@gmail.com>
2022-08-08 14:58:57 +0200

    refactor: add Vowels

--- src/App.tsx
+++ src/App.tsx
@@ -43,6 +43,7 @@ import { ArrowWord } from './components/ArrowWord';
 import { Mission } from './components/Mission';
 import { Tooltip } from './components/Tooltip';
 import { PhonemesTable } from './components/PhonemesTable';
+import { Vowels } from './components/Vowels';
 
 const quizLastIndex = QuizIndex.ShoppingForAPresent;
 
@@ -275,37 +276,6 @@ const App = () => {
         >
           Reset
         </button>
-      </div>
-    );
-  };
-
-  // TODO: create component
-  const renderVowels = () => {
-    return (
-      <div className="block-2">
-        <div className="article">
-          <h2 className="h3-title vowels-title">Vowels</h2>
-          <br />
-          <div>
-            Vowels are a set of unblocked sounds. They consist of the letters A, E, I, O, U (sometimes Y). The{' '}
-            <Tooltip definition={IPA_DEFINITION} word='IPA' /> lists 20 phonemes categorized as long, short, and{' '}
-            <a className="clickable-page" href="#diphthongs" onClick={() => setPageAndClear('diphthongs')}>
-              diphthongs
-            </a>
-            .
-          </div>
-          <br />
-          <div>
-            Vowels can sometimes be categorized as lax (short) and tense (long) depending on how much effort the lips
-            and tongue make when producing the sound.
-          </div>
-          <br />
-          <div>
-            Listed below are the phonemes that are widely used in the American English language. Some words might vary
-            in phonemes depending on regional dialects.
-          </div>
-          <PhonemesTable data={vowels} />
-        </div>
       </div>
     );
   };
@@ -968,7 +938,7 @@ const App = () => {
     }
 
     if (page === 'vowels') {
-      return renderVowels();
+      return <Vowels setPageAndClear={setPageAndClear} />;
     }
 
     if (page === 'falling') {
